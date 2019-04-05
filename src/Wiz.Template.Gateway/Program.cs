c6466f0dd4ffaf8e1e9a7550f85009aa283f5370
Lucas Mindello <lucasmindello@wizsolucoes.com.br>
2019-04-05 17:15:29 -0300

    First commit

---
+++ src/Wiz.Template.Gateway/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Wiz.Template.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile("configuration.json")
+                       .AddJsonFile($"configuration.{env.EnvironmentName}.json", true, true);
+
+                    config.AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>();
+    }
+}
---
+++ src/Wiz.Template.Gateway/Startup.cs
@@ -0,0 +1,59 @@
+﻿using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Wiz.Template.Gateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            void options(IdentityServerAuthenticationOptions identityServer)
+            {
+                identityServer.Authority = Configuration.GetSection("IdentityServer:Authority").Value;
+                identityServer.ApiName = Configuration.GetSection("IdentityServer:ApiName").Value;
+                identityServer.ApiSecret = Configuration.GetSection("IdentityServer:ApiSecret").Value;
+                identityServer.SupportedTokens = SupportedTokens.Both;
+            }
+
+            services.AddAuthentication().AddIdentityServerAuthentication(Configuration.GetSection("IdentityServer:ProviderKey").Value, options);
+            services.AddCors();
+            services.AddOcelot(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (!env.IsProduction())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors(x =>
+            {
+                x.AllowAnyOrigin();
+                x.AllowAnyHeader();
+                x.AllowAnyMethod();
+                x.AllowCredentials();
+            });
+            app.UseOcelot().Wait();
+        }
+    }
+}
